f42d9a287db6863dbd2e64bd2b7815f2b77aa52c
Valdomiro Neto <113729671+v4lds@users.noreply.github.com>
2022-09-17 19:00:13 -0300

    Update calculadora.c

--- C/calculadora.c
+++ C/calculadora.c
@@ -14,71 +14,70 @@ int main (){
 	printf("[2] Para executar uma subtracao.\n");
 	printf("[3] Para executar uma multiplicacao.\n");
 	printf("[4] Para executar uma divisao.\n");
-	scanf("%d",esc);
+	scanf("%d",&esc);
 	
-	C = (1);
-	R = (0);
+	C = 1;
+	R = 0;
 	
-	switch (esc)
+	switch (esc){
 		case 1:
 			printf("Quantos numeros deseja somar?\n");
-			scanf("%d",nsom);
+			scanf("%d",&nsom);
 			while (C <= nsom){
-				printf("digite o %d.o. numero:\n",C);
-				scanf("%f",num1);
+				printf("digite o %do. numero:\n",C);
+				scanf("%f",&num1);
 				R = R + num1;
 				C++;
-				break;
-			break;
 			}
-			printf("O resultado da soma e: %f\n",R);
+			printf("O resultado da 1soma e: %.2f.\n",R);
+			break;
 		case 2:
+			C = 2;
 			printf("Quantos numeros deseja subtrair?\n");
-			scanf("%d",nsom);
+			scanf("%d",&nsom);
+			printf("digite o 1o. numero: \n",C);
+			scanf("%f",&num1);
+			R = num1;
 			while (C <= nsom){
-				printf("digite o numero base:\n");
-				scanf("%f",num1);
-				printf("digite o %d.o. numero:\n",C);
-				scanf("%f",num2);
-				R = num1;
+				printf("digite o %do. numero: \n",C);
+				scanf("%f",&num2);
 				R = R - num2;
 				C++;
-				break;
 			}
-			printf("O resultado da subtracao e: %f\n",R);
-		break;
+			printf("O resultado da subtracao e: %.2f.\n",R);
+			break;
 		case 3:
+			C = 2;
 			printf("Quantos numeros deseja multiplicar?\n");
-			scanf("%d",nsom);
+			scanf("%d",&nsom);
+			printf("digite o 1o. numero: \n",C);
+			scanf("%f",&num1);
+			R = num1;
 			while (C <= nsom){
-				printf("digite o numero base:\n");
-				scanf("%f",num1);
-				printf("digite o %d.o. numero:\n",C);
-				scanf("%f",num2);
-				R = num1;
+				printf("digite o %do. numero: \n",C);
+				scanf("%f",&num2);
 				R = R * num2;
 				C++;
-				break;	
 			}
-			printf("O resultado da subtracao e: %f\n",R);
-		break;
+			printf("O resultado da multiplicacao e: %.2f.\n",R);
+			break;
 		case 4:
+			C = 2;
 			printf("Quantos numeros deseja dividir?\n");
-			scanf("%d",nsom);
+			scanf("%d",&nsom);
+			printf("digite o 1o. numero: \n",C);
+			scanf("%f",&num1);
+			R = num1;
 			while (C <= nsom){
-				printf("digite o numero base:\n");
-				scanf("%f",num1);
-				printf("digite o %d.o. numero\n",C);
-				scanf("%f",num2);
-				R = num1;
+				printf("digite o %do. numero: \n",C);
+				scanf("%f",&num2);
 				R = R / num2;
 				C++;
-				break;
-			break;
 			}
-			printf("O resultado da divisao e: %f\n",R);
-		break;
-		default
-			printf("O valor indicado e invalido");
- 	;}
+			printf("O resultado da divisao e: %.2f.\n",R);
+			break;
+		default:
+			printf("O valor inserido e invalido.");
+			break;
+	}		
 }
